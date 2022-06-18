21e21e3219b6932575e923ad987e27e9cd038a5e
Maryam <maryam.sa2009@gmail.com>
2022-06-18 17:17:33 +0430

    prime numbers

--- problemList_week4/week6/factorial_alternative.py
+++ problemList_week4/week6/factorial_alternative.py
@@ -1,11 +1,11 @@
-def factorial(n):
-    if n <= 1:
-        return 1
-    else:
-        return n * factorial(n-1)
+# def factorial(n):
+#     if n <= 1:
+#         return 1
+#     else:
+#         return n * factorial(n-1)
 
 
-# factorial alternative without using recursive
+# factorial alternative without using recursion
 def factor(num):
     fact = 1
     while num > 0:
@@ -14,8 +14,21 @@ def factor(num):
     return fact
 
 
+def factorial(n):
+    f = 1
+    while(n > 0):
+        f = f * n
+        n = n - 1
+        yield(n)
+
+
+print('=' * 40)
+
+
 if __name__ == '__main__':
-    print(factorial(10))
-    print(factor(10))
+
+    print(factor(5))
+    for number in factorial(5):
+        print(number)
 
 
